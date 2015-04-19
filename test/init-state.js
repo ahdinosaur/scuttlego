b636a88538793843071c032bd25250d84daac52d
Michael Williams <dinosaur@riseup.net>
2015-04-19 20:11:48 +1200

    init state with ndarray

--- test/init-state.js
+++ test/init-state.js
@@ -3,12 +3,15 @@ var Keys = require('ssb-keys')
 var range = require('lodash.range')
 var assign = require('lodash.assign')
 var pick = require('lodash.pick')
+var Ndarray = require('ndarray')
 
 var initState = require('../lib/init-state')
 
 var playerNames = ['Alice', 'Bob']
 
 test('init state', function (t) {
+  var rows = 19
+  var cols = 19
   var players = range(2).map(function (index) {
     return assign(
       Keys.generate(),
@@ -19,13 +22,17 @@ test('init state', function (t) {
   var initPlayers = pick(players, ['id', 'name'])
   var initMsg = {
     type: 'Init',
-    players: initPlayers
+    players: initPlayers,
+    rules: {
+      rows: rows,
+      cols: cols
+    }
   }
 
   var state = initState(initMsg)
   t.deepEqual(state, {
     players: initPlayers,
-    stones: new Map()
+    stones: new Ndarray([], [rows, cols])
   })
 
   t.end()
